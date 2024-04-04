0c7bc74afd23b9a930418c7293033b1b6a468a6b
j-Caripidis <a1762465@student.adelaide.edu.au>
2024-04-04 17:31:41 +1030

    files fixed

--- Finder.cpp
+++ Finder.cpp
@@ -1,37 +1,26 @@
-#include "Finder.h" 
+#include "Finder.h"
 #include <iostream>
-
-
-std::vector<int> concatenateVectors(std::vector<int>& v1, const std::vector<int>& v2) {
-    std::vector<int> result = v1;
-    result.insert(result.begin(), v2.begin(), v2.end());
-    return result;
-}
+#include <string>
+#include <chrono>
 
 std::vector<int> Finder::findSubstrings(std::string s1, std::string s2) {
-    std::vector<int> result;
+  std::vector<int> result;
+  int index = 1;
 
+  for (size_t i = 1; i <= s2.size(); i = index) { 
+    size_t found = s1.find(s2.substr(0, index));
 
-    if (s2.empty()) {
-        return result;
+    while (found != std::string::npos) {
+      result.push_back(found);
+      index++;
+      found = s1.find(s2.substr(0,index));
     }
 
-    
-
-    size_t found = s1.find(s2);
-    if (found != std::string::npos) {
-        result.push_back(found);
-    } else {
-        result.push_back(-1);
+    if (found == std::string::npos) {
+      result.push_back(-1);
+      index++;
     }
+  }
 
-    result = concatenateVectors(result, findSubstrings(s1, s2.substr(0, s2.size() - 1)));
-
-    // for(size_t i = 0; i < result.size(); i++) {
-    //     std::cout << result[i] << ", ";
-    // }
-    // std::cout << "" << std::endl;
-
-    return result;
+  return result;
 }
-
